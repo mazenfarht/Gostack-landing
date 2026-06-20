533b493b0223c6d78d13ee2d8049fe708235cec2
SalmaAboArab <salma.aboarab711@gmail.com>
2026-06-20 19:12:07 +0200

    minor fix

--- src/sections/index.tsx
+++ src/sections/index.tsx
@@ -1,11 +1,20 @@
+import ClientsLogos from "./ClientsLogos";
+import HeroSection from "./HeroSection";
+import Navbar from "./Navbar";
+import Partners from "./Partners";
 import Section2 from "./section2";
 import Section3 from "./section3";
 
 export default function LandingPage() {
   return (
     <div>
+      <Navbar />
+      <HeroSection />
+      <ClientsLogos />
       <Section2 />
+      <Partners />
       <Section3 />
+      <footer />
     </div>
-  )
+  );
 }
